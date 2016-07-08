36f2913ee1be5c7223a76cb43cf2c7753aee90f2
AJ Heller <aj@drfloob.com>
2016-07-07 21:04:43 -0700

    flesh out basic structures and some algorithms/libraries
    also fix the build to include boost date_time stuff, which I've found
    is not header-only.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This project was built on ArchLinux (3.18.1-1, x86_64) with the following depend
 CMake v3.5.2
 GNU Make v4.2.1
 GNU G++ v6.1.1 20160602
-Boost v1.60.0 (MultiIndex)
+Boost v1.60.0 (MultiIndex, Posix Time)
 JsonCpp v1.7.2
 
 C++11 (required by JsonCp v1.y.z)
--- src/naive_exploration.cpp
+++ src/naive_exploration.cpp
@@ -3,56 +3,113 @@
 #define BOOST_MULTI_INDEX_ENABLE_SAFE_MODE
 #endif
 
-
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/identity.hpp>
 #include <boost/multi_index/member.hpp>
-
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
 #include <iostream>
 #include <string>
+#include <unordered_set>
+#include <functional>
+
 #include <json/json.h>
 
+/*------------------------------------------------------------------------------
+Payments are streamed in, parsed into timestamped connections. 
 
-struct employee
+The set of connections by user (userConnections) is located for each user.
+
+The new connection is added if not already present, otherwise the timestamp is 
+updated.
+------------------------------------------------------------------------------*/
+
+struct payment
 {
-  int         id;
-  std::string name;
+  std::string actor;
+  std::string target;
+  boost::posix_time::ptime time;
 
-  employee(int id, const std::string& name)
-    :id(id), name(name)
-  {}
-
-  bool operator < (const employee& e) const
+  payment(const std::string& actor_, const std::string& target_, const std::string& time_)
+    : target(target_), actor(actor_)
   {
-    return id < e.id;
+    // 2014-03-27T04:28:20Z
+    std::stringstream ss(time_);
+    boost::posix_time::time_input_facet *dif = new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ");
+    ss.imbue(std::locale(ss.getloc(), dif));
+
+    ss >> time;
+    std::cout << time << std::endl;
   }
 
-  friend std::ostream & operator << (std::ostream &outStream, const employee &e ) {
-    return outStream << e.name << " : " << e.id << std::endl;
-  }
 };
 
-struct id{};
-struct name{};
+struct connection
+{
+  std::string target;
+  boost::posix_time::ptime time;
+
+  connection(const payment& p) :target(p.target), time(p.time) {}
+
+
+  // Within a specific user's set of connections, the other party's
+  // name is sufficient to determine connection equality.
+  bool operator == (const connection& c2) const {
+    return target == c2.target;
+  }
+  
+  struct Hash {
+    size_t operator () (const connection& c) const {
+      return std::hash<const char *>()(c.target.c_str());
+      // return std::hash<const std::string>()(c.target);
+    }
+  };
+
+
+  friend std::ostream& operator << (std::ostream &out, const connection& c)
+  {
+    out << "[" << c.target << "]";
+    return out;
+  }
+    
+};
+
+
+struct userConnections
+{
+  std::string actor;
+  std::unordered_set<connection, connection::Hash> connections;
+
+  userConnections(const payment& p)
+    : actor(p.actor)
+  {
+    connections.insert(connection(p));
+  }
+
+  friend std::ostream& operator << (std::ostream &out, const userConnections& uc)
+  {
+    out << uc.actor << " (" << uc.connections.size() << " connections)" << std::endl;
+    return out;
+  }
+
+};
+
+struct actor{};
+struct target{};
+struct time{};
 
 
 // define a multiply indexed set with indices by id and name
 typedef boost::multi_index_container<
-  employee,
+  userConnections,
   boost::multi_index::indexed_by<
-    // sort by employee::operator<
     boost::multi_index::ordered_unique<
-      boost::multi_index::tag<id>, BOOST_MULTI_INDEX_MEMBER(employee, int, id)
-      >,
-    
-    // sort by less<string> on name
-    boost::multi_index::ordered_non_unique<
-      boost::multi_index::tag<name>, BOOST_MULTI_INDEX_MEMBER(employee, std::string, name)
+      boost::multi_index::tag<actor>, BOOST_MULTI_INDEX_MEMBER(userConnections, std::string, actor)
       >
     >
-  > employee_set;
+  > connection_set;
 
 
 template<typename Tag, typename MultiIndexContainer>
@@ -68,18 +125,23 @@ void print_out_by(const MultiIndexContainer& es)
 
 
 int main() {
-  employee_set es;
-  es.insert(employee(2, "george"));
-  es.insert(employee(1, "paul"));
-  es.insert(employee(3, "ringo"));
-  es.insert(employee(0, "john"));
-  es.insert(employee(-1, "yoko"));
+  connection_set cs;
 
-  print_out_by<name>(es);
+  payment p("Jordan-Gruber", "Jamie-Korn", "2014-03-27T04:28:20Z");
+  cs.insert(userConnections(p));
 
-  std::cout << "\n";
+  p= payment("Maryann-Berry", "Jamie-Korn", "2016-04-07T03:33:19Z");
+  cs.insert(userConnections(p));
+
+  p= payment("Ying-Mo", "Maryann-Berry", "2016-04-07T03:33:19Z");
+  cs.insert(userConnections(p));
   
-  print_out_by<id>(es);
+  p= payment("Ying-Mo", "FartButt", "2016-04-07T03:44:19Z");
+  cs.insert(userConnections(p));
+  
+
+  print_out_by<actor>(cs);
+
   
   return 0;
 }
